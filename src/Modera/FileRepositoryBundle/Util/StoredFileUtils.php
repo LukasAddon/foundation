2f967103a07a34e8127ca8a53d1f33b8e02a1492
Sergei Vizel <sergei.vizel@modera.com>
2025-10-17 08:58:15 +0300

    [ModeraFileRepositoryBundle] improvements

--- src/Modera/FileRepositoryBundle/Util/StoredFileUtils.php
+++ src/Modera/FileRepositoryBundle/Util/StoredFileUtils.php
@@ -9,10 +9,11 @@ class StoredFileUtils
 {
     public static function formatFileSize(int $size): string
     {
-        $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
         $power = $size > 0 ? \floor(\log($size, 1024)) : 0;
+        $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        $index = (int) $power;
 
-        return \number_format($size / \pow(1024, $power), 2, '.', ',').' '.$units[$power];
+        return \number_format($size / \pow(1024, $power), 2, '.', ',').' '.$units[$index];
     }
 
     final private function __construct()
